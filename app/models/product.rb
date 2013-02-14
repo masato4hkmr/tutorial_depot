69ba2b2b2c14de893cc4692a7c13cdaf6c1b26c5
tb_ruby <exit>
2013-02-14 15:47:42 +0900

    'StoreController'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 #encoding: utf-8
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: {in: true, message: "is required"}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:     %r{\.(gif|jpg|png)$}i,
     message:  'はGIF、JPG、PNG画像のURLでなければなりません'
   }
+  validates :title, length: {minimum: 10}
 end
