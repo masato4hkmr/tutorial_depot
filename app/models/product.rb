74c240e1f4df428b4621626be51ff60b43340cdc
tb_ruby <exit>
2013-02-15 09:12:26 +0900

    Add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 #encoding: utf-8
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: {in: true, message: "is required"}
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -8,4 +12,16 @@ class Product < ActiveRecord::Base
     message:  'はGIF、JPG、PNG画像のURLでなければなりません'
   }
   validates :title, length: {minimum: 10}
+  
+  private
+  
+  # この商品を参照している品目がないことを確認する
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, '品目が存在します')
+      return false
+    end
+  end
 end
